8adc732b7a0991858bd0480c47860f00c3a04eab
Nick-Levin <kolya666kv@gmail.com>
2020-01-24 19:16:04 +0200

    jenkinfile

--- src/test/java/org/PasswordGeneratorTest.java
+++ src/test/java/org/PasswordGeneratorTest.java
@@ -1,5 +1,6 @@
 package org;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.util.PasswordGenerator;
 
@@ -8,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PasswordGeneratorTest {
 
     @Test
+    @DisplayName("Generator Test")
     public void generatorTest() {
         final int length = 50;
         final String pass = PasswordGenerator.generatePassword(length);
